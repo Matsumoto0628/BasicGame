269aea1bb77ccfebabdf7d3d7e42e0bc9307e001
Matsumoto0628 <kanikani0628@gmail.com>
2025-12-19 12:52:53 +0900

    add:ビルド時間短縮のためプリコンパイル済みヘッダーを追加

---
+++ basic_game/scripts/pch.h
@@ -0,0 +1,8 @@
+#pragma once 
+
+#include <d3d11.h>
+#pragma comment(lib, "d3d11.lib")
+#include <DirectXMath.h>
+#define DX_SAFE_RELEASE(x) { if(x) { (x)->Release(); (x)=nullptr; } }
+
+class Render;
