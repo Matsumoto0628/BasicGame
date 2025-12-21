cb266da4b87b6ec3692428b256e2f85213d086c4
Matsumoto0628 <kanikani0628@gmail.com>
2025-12-21 22:31:26 +0900

    fix:プリコンパイル済みヘッダーのincludeを削除、循環参照を防ぐため

--- basic_game/scripts/pch.h
+++ basic_game/scripts/pch.h
@@ -1,8 +1,8 @@
 #pragma once 
 
 #include <d3d11.h>
+#include <d3dcompiler.h>
 #pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
 #include <DirectXMath.h>
-#include "vertex.h"
-#include "renderer.h"
 #define DX_SAFE_RELEASE(x) { if(x) { (x)->Release(); (x)=nullptr; } }
